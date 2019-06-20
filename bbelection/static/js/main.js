42571a2355595786c3c88f333315fb5d36c8ba13
ynjacobs <ytj64611@gamil.com>
2019-06-20 13:46:48 -0400

    makes ajax request to populate page with name and number of votes in li

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    // In the election.js file, build an AJAX request that makes a GET request to the root path, 
+    // and expects a JSON response.
+    axios.get("https://bb-election-api.herokuapp.com/")
+    .then(response => {
+        let data = response.data.candidates;
+        console.log(data);
+        let ul = document.querySelector('#candidates');
+        
+        data.forEach(candidate => {
+            let li = document.createElement('li');
+            let name = candidate.name;
+            let votes = candidate.votes;
+            li.innerText = `${name}, ${votes}`;
+            ul.appendChild(li);
+            
+            
+        });
+        
+    })
+    .catch(error => {
+        console.log('error', error);
+        
+    })
 });
