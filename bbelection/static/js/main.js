daa4a486b436a5ad92a19c0d75584233068e5345
ynjacobs <ytj64611@gamil.com>
2019-06-21 13:11:12 -0400

    makes post request and updates the vote count when refreshed and disables vote buttons

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -10,17 +10,56 @@ document.addEventListener("DOMContentLoaded", function() {
         
         data.forEach(candidate => {
             let li = document.createElement('li');
+            let form = document.createElement('form');
+            let btn = document.createElement('button');
+            let hdnInput = document.createElement('input');
+            hdnInput.type = 'hidden';
+            hdnInput.name = 'name';
+            hdnInput.value = candidate.name;
+            form.method = 'POST';
+            form.action = "https://bb-election-api.herokuapp.com/vote";
+            btn.innerText = 'Vote!';
+
+
+            
             let name = candidate.name;
             let votes = candidate.votes;
-            li.innerText = `${name}, ${votes}`;
+            let zText = `${name}, ${votes}`
+            // li.innerHTML = form;
+            form.appendChild(btn);
+            form.appendChild(hdnInput);
+            let textNode = document.createTextNode(zText);
+            li.appendChild(textNode);
+            li.appendChild(form);
             ul.appendChild(li);
-            
-            
+            // li.innerText = zText;
+
+            console.log(form);
+            // console.log(btn);
+            form.addEventListener('submit', e => {
+                e.preventDefault();
+                console.log('i am submitted');
+                axios.post('https://bb-election-api.herokuapp.com/vote', { name})
+                .then(response => {
+
+document.querySelectorAll('button').forEach(e => {e.disabled = 'true'});
+
+                })
+                .catch(e => {});
+                
+                
+                });
+
         });
-        
+
+
+
+
     })
     .catch(error => {
         console.log('error', error);
         
-    })
+    });
+
+
 });
